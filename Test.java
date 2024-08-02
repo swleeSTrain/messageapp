ffe547897e13c5664e2d39a187180ad31e034150
seonjin <wlq862@naver.com>
2024-08-02 17:30:54 +0900

    pull test

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("하이요");
+        System.out.println("dfdf");
     }
 }
