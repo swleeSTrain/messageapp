0267f26d1f011668456b0b1130431c8c075763c1
seonjin <wlq862@naver.com>
2024-08-07 15:12:12 +0900

    뷰컨트롤러 수정

--- src/main/java/org/example/messagewebapp/controller/ReceiveListViewController.java
+++ src/main/java/org/example/messagewebapp/controller/ReceiveListViewController.java
@@ -3,10 +3,12 @@ package org.example.messagewebapp.controller;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
+import org.example.messagewebapp.common.CookieUtil;
 import org.example.messagewebapp.dao.MessageDAO;
 import org.example.messagewebapp.vo.MessageVO;
 
@@ -18,11 +20,15 @@ import java.util.List;
 public class ReceiveListViewController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String user_id = req.getParameter("user_id");
+        Cookie userCookie = CookieUtil.getCookie(req, "user_id");
+        if (userCookie == null) {
+            resp.sendRedirect("/login");
+        }
+        String user_id = userCookie.getValue();
         try {
-            List<MessageVO> all_messages = MessageDAO.INSTANCE.getAllMessage("std1");
+            List<MessageVO> all_messages = MessageDAO.INSTANCE.getReceiveMessage(user_id);
             req.setAttribute("recive_messages", all_messages);
-            req.getRequestDispatcher("/WEB-INF/test/receivelist.jsp").forward(req, resp);
+            req.getRequestDispatcher("/WEB-INF/view/receivelist.jsp").forward(req, resp);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/example/messagewebapp/controller/SendListViewController.java
+++ src/main/java/org/example/messagewebapp/controller/SendListViewController.java
@@ -3,10 +3,12 @@ package org.example.messagewebapp.controller;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
+import org.example.messagewebapp.common.CookieUtil;
 import org.example.messagewebapp.dao.MessageDAO;
 import org.example.messagewebapp.vo.MessageVO;
 
@@ -18,12 +20,16 @@ import java.util.List;
 public class SendListViewController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie userCookie = CookieUtil.getCookie(req, "user_id");
+        if (userCookie == null) {
+            resp.sendRedirect("/login");
+        }
+        String user_id = userCookie.getValue();
 
-        String user_id = req.getParameter("user_id");
         try {
-            List<MessageVO> all_messages = MessageDAO.INSTANCE.getAllMessage("std1");
+            List<MessageVO> all_messages = MessageDAO.INSTANCE.getSendMessage(user_id);
             req.setAttribute("send_messages", all_messages);
-            req.getRequestDispatcher("/WEB-INF/test/sendlist.jsp").forward(req, resp);
+            req.getRequestDispatcher("/WEB-INF/view/sendlist.jsp").forward(req, resp);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
